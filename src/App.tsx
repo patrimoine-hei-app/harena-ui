8ca57796f2f84d6abbd1377eb8cc3f060fda88fc
KevinDonovan2 <hei.donovan.2@gmail.com>
2024-07-20 22:04:49 +0300

    feat: section patrimoine

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
-import MyAdmin from "./admin";
+import { Admin, Resource } from 'react-admin';
+import UserIcon from '@mui/icons-material/Group';
+import jsonServerProvider from "ra-data-json-server";
+import { PatrimoineCreate } from './components/patrimoine/patrimoineCreate';
+import { PatrimoineList } from './components/patrimoine/patrimoineList';
+import { PatrimoineEdit } from './components/patrimoine/patrimoineEdit';
 
-const App = () => <MyAdmin />;
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+const App = () => (
+    <Admin dataProvider={dataProvider} >
+        <Resource
+            name="Patrimoine"
+            list={PatrimoineList}
+            create={PatrimoineCreate}
+            edit={PatrimoineEdit}
+            icon={UserIcon}
+        />
+    </Admin>
+);
 
 export default App;
---
+++ src/components/patrimoine/patrimoineList.tsx
@@ -0,0 +1,11 @@
+import { List, Datagrid, TextField } from 'react-admin';
+
+export const PatrimoineList: React.FC = (props) => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+            <TextField source="nom" />
+            <TextField source="type" />
+            <TextField source="valeur_comptable" />
+        </Datagrid>
+    </List>
+);
