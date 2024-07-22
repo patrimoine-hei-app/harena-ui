b908df84bf765dce47c64f64c5cf76c502434e5c
Lovatiana15 <hei.lovatiana@gmail.com>
2024-07-22 09:22:46 +0300

    feat:  create possession component

---
+++ src/components/possession/possession.tsx
@@ -0,0 +1,12 @@
+import PossessionList from './possessionList';
+import PossessionCreate from './possessionCreate';
+import PossessionEdit from './possessionEdit';
+
+const Possession = {
+    list: PossessionList,
+    create: PossessionCreate,
+    edit: PossessionEdit
+};
+export default Possession;
+
+
---
+++ src/components/possession/possessionCreate.tsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import {
+    Create,
+    SimpleForm,
+    TextInput,
+    SelectInput,
+    NumberInput,
+    ReferenceInput,
+    DateInput,
+    CreateProps
+} from 'react-admin';
+import { useTypeChange } from '../hook/useTypeChange';
+
+const PossessionCreate: React.FC<CreateProps> = (props) => {
+    const { type, handleTypeChange } = useTypeChange();
+
+    return (
+        <Create {...props}>
+            <SimpleForm>
+                <TextInput source="nom" />
+                <SelectInput
+                    source="type"
+                    choices={[
+                        { id: 'ARGENT', name: 'Argent' },
+                        { id: 'MATERIEL', name: 'Matériel' },
+                        { id: 'FLUXARGENT', name: 'Flux d\'Argent' },
+                    ]}
+                    onChange={(event) => handleTypeChange(event as React.ChangeEvent<HTMLSelectElement>)}
+                />
+                {type === 'ARGENT' && (
+                    <>
+                        <SelectInput
+                            source="argent.type"
+                            choices={[
+                                { id: 'DETTE', name: 'Dette' },
+                                { id: 'CREANCE', name: 'Créance' },
+                                { id: 'ESPECES', name: 'Espèces' },
+                                { id: 'AUTRES', name: 'Autres' },
+                            ]}
+                        />
+                        <DateInput source="argent.date_d_ouverture" />
+                    </>
+                )}
+                {type === 'MATERIEL' && (
+                    <>
+                        <DateInput source="materiel.date_d_acquisition" />
+                        <NumberInput source="materiel.taux_dappreciation_annuel" />
+                    </>
+                )}
+                {type === 'FLUXARGENT' && (
+                    <>
+                        <ReferenceInput source="flux_argent.argent" reference="argents">
+                            <SelectInput optionText="nom" />
+                        </ReferenceInput>
+                        <DateInput source="flux_argent.debut" />
+                        <DateInput source="flux_argent.fin" />
+                        <NumberInput source="flux_argent.flux_mensuel" />
+                        <NumberInput source="flux_argent.date_d_operation" />
+                    </>
+                )}
+                <NumberInput source="valeur_comptable" />
+            </SimpleForm>
+        </Create>
+    );
+};
+
+export default PossessionCreate;
