b513f6a93d1b416c8acd5eeb61b9788fad542c63
KevinDonovan2 <hei.donovan.2@gmail.com>
2024-07-28 13:59:48 +0300

    feat: projection future

---
+++ src/components/projectionFuture/ProjectionFutureCreate.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  Create,
+  SimpleForm,
+  TextInput,
+  NumberInput,
+  DateInput,
+} from "react-admin";
+
+export const ProjectionFutureCreate: React.FC = (props) => (
+  <Create {...props}>
+    <SimpleForm>
+      <TextInput source="nom_argent" label="Nom Argent" />
+      <DateInput source="date" label="Date" />
+      <NumberInput source="valeur_argent" label="Valeur Argent" />
+      <DateInput source="flux_argents[0].t" label="Flux Date" />
+      <TextInput source="flux_argents[0].nom" label="Flux Nom" />
+      <NumberInput
+        source="flux_argents[0].valeur_comptable"
+        label="Flux Valeur Comptable"
+      />
+      <TextInput source="flux_argents[0].devise.nom" label="Flux Devise Nom" />
+      <TextInput
+        source="flux_argents[0].devise.code"
+        label="Flux Devise Code"
+      />
+    </SimpleForm>
+  </Create>
+);
